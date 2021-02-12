211c4705d85df8c4836be5a6096b4ac66d0af518
SemenTsvolko <75906654+SemenTsvolko@users.noreply.github.com>
2021-02-12 19:42:38 +0300

    Add files via upload

---
+++ 2 rabota/2 rabota/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_rabota
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите вещественное число z");
+            double z = double.Parse(Console.ReadLine());
+            double r1 = Math.Pow(z, 3);
+            Console.WriteLine( "Введите число x" );
+            double x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите число y");
+            double y= double.Parse(Console.ReadLine());
+            Console.WriteLine("Сумма куба z и максимального из чисел x и y ");
+            double r2 = Math.Max(x, y);
+            double r3 = (r1 + r2);
+            Console.WriteLine(r3);
+
+            Console.ReadKey();
+        }
+    }
+}
